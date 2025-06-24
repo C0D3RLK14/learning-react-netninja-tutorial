cb3e8623b91277b79781d597655282db4b944ce5
C0D3RLK14 <lk12karunanayaka@gmail.com>
2025-06-24 12:21:56 +0530

    Clean up

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,16 +1,11 @@
 import './App.css';
-// importing the 'Navbar' component
 import Navbar from './Navbar';
 import Home from './Home';
 
 function App() {
   return (
     <div className="App">
-      {/* Adding the Navbar component into the JSX template */}
-      {/* Method 1 : Self closing tags */}
       <Navbar /> 
-      {/* Method 2 : Open and close tags
-      <Navbar> </Navbar>  */}
       <div className="content">
         <Home />
       </div>
