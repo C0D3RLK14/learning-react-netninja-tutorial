704d8d3723b8489762201b623136c30f945ce2ad
C0D3RLK14 <lk12karunanayaka@gmail.com>
2025-06-24 12:21:15 +0530

    Created and add components to the App.js
    We created a Navbar and Home component. By first creating a Home.js and Navbar.js files. And adding a stateless functional component in them using sfc snippet. And coded the return JSX template. Then exported them 'export default Home;' and imported them in the App.js by 'import Home from './Home'' and added the component to the JSX template using <Home/>

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+// importing the 'Navbar' component
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
   return (
     <div className="App">
+      {/* Adding the Navbar component into the JSX template */}
+      {/* Method 1 : Self closing tags */}
+      <Navbar /> 
+      {/* Method 2 : Open and close tags
+      <Navbar> </Navbar>  */}
       <div className="content">
-        <h1>App Component</h1>
+        <Home />
       </div>
     </div>
   );
