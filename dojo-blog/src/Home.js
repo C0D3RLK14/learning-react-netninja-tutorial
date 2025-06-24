456504d25b176b608313d9cd16d0268c698645a4
C0D3RLK14 <lk12karunanayaka@gmail.com>
2025-06-24 23:22:03 +0530

    Clean up

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -3,26 +3,12 @@ import { useState } from 'react';
 
 const Home = () => {
 
-    // This does not change the displayed value of mario to luigi when the button is clicked
-    // let name = 'mario';
-
-    // const handleClick = () => {
-    //     name = 'luigi';
-    //     console.log(name);
-    // };
-
-    // Inorder to change the value displayed when the button is clicked we would have to use the useState function.
     const [name, setName] = useState('mario');
-    // NOTE: This [name, setName] is known as 'Array destructuring', and according to the above statement we will be creating a const named 'name' with the value in the useState hook 'mario' (therefore, const name = 'mario'). And creating a function 'setName' to change the value of the const 'name' which will be called later in the code.
-
     const [age, setAge] = useState(25);
-    // The 'useState()' hook could used with any datatype.
 
     const handleClick = () => {
-        // Calling the function to change the value of name when click event occurs
         setName('luigi');
         setAge(40);
-        console.log(name, age);
     };
 
     return (  
