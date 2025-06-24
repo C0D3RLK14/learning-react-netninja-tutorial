edfa996336385dad76034b623ef18ef116294ea3
C0D3RLK14 <lk12karunanayaka@gmail.com>
2025-06-24 23:46:37 +0530

    Clean up

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -2,20 +2,8 @@
 import { useState } from 'react';
 
 const Home = () => {
-
-    const [name, setName] = useState('mario');
-    const [age, setAge] = useState(25);
-
-    const handleClick = () => {
-        setName('luigi');
-        setAge(40);
-    };
-
     return (  
         <div className="home">
-            <h1>Homepage</h1>
-            <p>{ name } is { age } years old.</p>
-            <button onClick={handleClick}>Click me</button>
         </div>
     );
 }
