e95340dc3643aad31f1265bf99075d0cee1ee0c1
C0D3RLK14 <lk12karunanayaka@gmail.com>
2025-06-24 23:20:37 +0530

    useState hook
    Imported the useState hook and used it to add reactive elements

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,16 +1,35 @@
+// importing useState
+import { useState } from 'react';
+
 const Home = () => {
-    const handleClick = () => console.log('hello, ninjas');
-    
-    const handleClickMeAgain = (name, e) => {
-        console.log('hello,', name);
-        console.log(e);
-    }
+
+    // This does not change the displayed value of mario to luigi when the button is clicked
+    // let name = 'mario';
+
+    // const handleClick = () => {
+    //     name = 'luigi';
+    //     console.log(name);
+    // };
+
+    // Inorder to change the value displayed when the button is clicked we would have to use the useState function.
+    const [name, setName] = useState('mario');
+    // NOTE: This [name, setName] is known as 'Array destructuring', and according to the above statement we will be creating a const named 'name' with the value in the useState hook 'mario' (therefore, const name = 'mario'). And creating a function 'setName' to change the value of the const 'name' which will be called later in the code.
+
+    const [age, setAge] = useState(25);
+    // The 'useState()' hook could used with any datatype.
+
+    const handleClick = () => {
+        // Calling the function to change the value of name when click event occurs
+        setName('luigi');
+        setAge(40);
+        console.log(name, age);
+    };
 
     return (  
         <div className="home">
             <h1>Homepage</h1>
+            <p>{ name } is { age } years old.</p>
             <button onClick={handleClick}>Click me</button>
-            <button onClick={() => handleClickMeAgain('LK')}>Click me again</button>
         </div>
     );
 }
