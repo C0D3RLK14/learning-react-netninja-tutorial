7e4b210c5abac04cbb21832afad3471ecb4090b8
C0D3RLK14 <lk12karunanayaka@gmail.com>
2025-06-27 10:04:41 +0530

    Clean up

--- dojo-blog/src/BlogList.js
+++ dojo-blog/src/BlogList.js
@@ -1,10 +1,3 @@
-// Method 1 to use props
-// Getting values from props. Need to pass the 'props' argument in order to access it.
-// const BlogList = (props) => {
-    // Saving data from props into variables
-
-// Method 2 to using props
-// We could destructure the props values into the argument
 const BlogList = ({ blogs, title }) => {
     return (  
         <div className="blog-preview">
--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-// import Blog from './BlogList'
 import BlogList from './BlogList'
 
 const Home = () => {
@@ -11,17 +10,6 @@ const Home = () => {
 
     return (  
         <div className="home">
-            {/* { blogs.map((blog) => (
-                <div className="blog-preview" key={blog.id}>
-                    <h2>{ blog.title }</h2>
-                    <p>by { blog.author }</p>
-                </div>
-            ))} // Moved to Blogs.js */ }
-
-            {/* Nesting the BlogList component */}
-            {/* <Blog blogs={blogs}/> //So apparently we don't need match the exported variable name 'BlogList' when importing the same component with  another name like 'Blog' */}
-            
-            {/* Passing values as props "blogs={blogs} title="All Blogs"" */}
             <BlogList blogs={blogs} title="All Blogs"/>
         </div>
     );
