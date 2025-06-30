187ee23577a62850f579ec63ab838ede6c4dbec3
C0D3RLK14 <lk12karunanayaka@gmail.com>
2025-06-30 22:49:24 +0530

    Clean up

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -3,23 +3,11 @@ import BlogList from './BlogList'
 
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
-    // Creating a boolean data store to use as a condition to run code if the data are still being fetched
     const [isPending, setIsPending] = useState(true);
 
     useEffect(() => {
         console.log('useEffect with an empty dependency array invoked');
 
-        // fetch('http://localhost:8000/blogs')
-        // .then(res => {
-        //     return res.json();
-        // })
-        // .then(data => {
-        //     setBlogs(data);
-        //     // setting the 'isPending' status as data is fetched
-        //     setIsPending(false);
-        // });
-
-        // Emulating real-world data fetching scenario(which takes time)
         setTimeout(() => {
             fetch('http://localhost:8000/blogs')
             .then(res => {
@@ -27,7 +15,6 @@ const Home = () => {
             })
             .then(data => {
                 setBlogs(data);
-                // setting the 'isPending' status as data is fetched
                 setIsPending(false);
             });
         }, 1000);
@@ -35,8 +22,8 @@ const Home = () => {
 
     return (  
         <div className="home">
-            {/* Using a conditional statement to displaying 'loading' if the data are still being fetched */}
             { isPending && <div>Loading...</div> }
+            
             { blogs && <BlogList blogs={blogs} title="All Blogs" /> }
             {blogs && <BlogList blogs={blogs.filter((blogs) => blogs.author === 'mario')} title="Mario's Blogs"/> }
         </div>
