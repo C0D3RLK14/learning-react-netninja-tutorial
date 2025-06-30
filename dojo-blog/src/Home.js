7dd7afaab7f7e0bf00711b5f37937f3ed632c063
C0D3RLK14 <lk12karunanayaka@gmail.com>
2025-07-01 00:47:51 +0530

    Clean up

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -13,10 +13,6 @@ const Home = () => {
         setTimeout(() => {
             fetch('http://localhost:8000/blogs')
             .then(res => {
-                // Logging the response object
-                console.log(res);
-
-                // NOTE: The response object has a property named 'ok' we could use this to find out if we are getting data back from the JSON Server and run a block of code accordingly. Therefore we'll throw an error if data not received as follows.
                 if(!res.ok){
                     throw Error('Could not fetch data for that resource.');
                 }
@@ -28,28 +24,19 @@ const Home = () => {
                 setIsPending(false);
             })
             .catch(err => {
-                // Logging the error message, normally the catch block would be run in case of a network error (server not responding)
-                console.log(err.message);
-                // Saving error message to 'error' variable 
                 setError(err.message);
-                // To stop displaying 'Loading' if there is an error
                 setIsPending(false);
-
-                // Testing if the catch block is run
-                console.log('Catch block ran.');
             });
         }, 1000);
     }, []);
 
     return (  
         <div className="home">
-            {/* Displaying context if there is an error */}
             { error && <div>{ error }</div> }
 
             { isPending && <div>Loading...</div> }
 
             { blogs && <BlogList blogs={blogs} title="All Blogs" /> }
-            {/* {blogs && <BlogList blogs={blogs.filter((blogs) => blogs.author === 'mario')} title="Mario's Blogs"/> } */}
         </div>
     );
 }
