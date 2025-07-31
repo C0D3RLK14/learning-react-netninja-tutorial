596211461124141f5715094219cb00122e7b6104
C0D3RLK14 <lk12karunanayaka@gmail.com>
2025-07-31 21:57:12 +0530

    Finished The react router
    So we set up the router for the home page. Unfortunately had to uninstall react-router using 'npm uninstall react-router' to install the v5 of react router using the same command as before to install v6 just changing the ver no. to 5.

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,14 +1,26 @@
 import Navbar from './Navbar';
 import Home from './Home';
 
+// Importing components from the react-router
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 function App() {
   return (
-    <div className="App">
-      <Navbar /> 
-      <div className="content">
-        <Home />
+    // Surrounding the 'div' element with the Router
+    <Router>
+      <div className="App">
+        <Navbar /> 
+        <div className="content">
+          {/* Routing the path '/' to the home page */}
+          {/* <Home /> */}
+          <Switch>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div> 
       </div>
-    </div>
+    </Router>
   );
 }
 
